9caf902dbc45936ed7192825db127112b7f5842f
ShogoNakao <knpy0508@gmail.com>
2024-02-05 15:21:39 +0900

    ダイアログを「戻す」ボタンのテキストを「閉じる」から「戻る」に変更しました

--- todo_react/src/components/Form/FormButton.jsx
+++ todo_react/src/components/Form/FormButton.jsx
@@ -4,7 +4,7 @@ export const FormButton = ({ onClose }) => {
   return (
       <div className="end_button">
         <button type="submit">送信</button>
-        <button type="button" onClick={onClose}>閉じる</button>
+        <button type="button" onClick={onClose}>戻る</button>
       </div>
   );
 };
